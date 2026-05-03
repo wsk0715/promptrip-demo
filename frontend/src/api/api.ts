949fd979068c273134160713e0cdd76a5d6e0e1a
wsk0715 <leindey@gmail.com>
2026-05-03 14:56:56 +0900

    Change preferences

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL = import.meta.env.VITE_API_URL;
+export const BASE_URL = import.meta.env.VITE_PUBLIC_API_URL;
 
 const api = axios.create({
   baseURL: BASE_URL,
